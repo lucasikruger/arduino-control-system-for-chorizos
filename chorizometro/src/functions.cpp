f987657177d200759d5fc31aacda75f7fe205f9e
lucaskruger10 <lucaskruger10@gmail.com>
2022-07-31 19:40:31 -0300

    fixed the overflowstate

--- chorizometro/src/functions.cpp
+++ chorizometro/src/functions.cpp
@@ -24,26 +24,37 @@ void keyboardActionWith(Menu &menu)
   }
 }
 
-// -------------------------------------------
-
+// ------------------------------------------
+bool overFlowStateTemp = false;
+bool overFlowStateHum = false;
 void setRelays(int temp, int valTemp, int varTemp, int hum, int valHum, int varHum)
 {
+
   int relay[4] = {2, 3, 11, 12};
   // Temp relays
   if (temp < (valTemp - varTemp)) // if temp is lower
   {
     digitalWrite(relay[0], LOW);
     digitalWrite(relay[1], HIGH);
+    overFlowStateTemp = true;
   }
   if (temp > (valTemp + varTemp)) // higher
   {
     digitalWrite(relay[0], HIGH);
     digitalWrite(relay[1], LOW);
+    overFlowStateTemp = true;
   }
   if (temp >= (valTemp - varTemp) && temp <= (valTemp + varTemp)) // in range
   {
-    digitalWrite(relay[0], HIGH);
-    digitalWrite(relay[1], HIGH);
+    if (!overFlowStateTemp)
+    {
+      digitalWrite(relay[0], HIGH);
+      digitalWrite(relay[1], HIGH);
+    }
+    if (temp == valTemp)
+    {
+      overFlowStateTemp = false;
+    }
   }
 
   // Hum relays
@@ -51,15 +62,25 @@ void setRelays(int temp, int valTemp, int varTemp, int hum, int valHum, int varH
   {
     digitalWrite(relay[2], LOW);
     digitalWrite(relay[3], HIGH);
+    overFlowStateHum = true;
   }
   if (hum > (valHum + varHum)) // higher
   {
     digitalWrite(relay[2], HIGH);
     digitalWrite(relay[3], LOW);
+    overFlowStateHum = true;
   }
   if (hum >= (valHum - varHum) && hum <= (valHum + varHum)) // in range
   {
-    digitalWrite(relay[2], HIGH);
-    digitalWrite(relay[3], HIGH);
+
+    if (!overFlowStateTemp)
+    {
+      digitalWrite(relay[2], HIGH);
+      digitalWrite(relay[3], HIGH);
+    }
+    if (hum == valHum)
+    {
+      overFlowStateHum = false;
+    }
   }
 }
